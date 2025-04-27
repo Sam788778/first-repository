b7454bd6f822ba75e5207212d4fe8ed02d6ae1c9
Sam788778 <samvelabrahamyan566@gmail.com>
2025-04-27 21:24:14 +0300

    new buttons

--- my-app/src/components/ProductsCard/ProductsCard.jsx
+++ my-app/src/components/ProductsCard/ProductsCard.jsx
@@ -1,16 +1,44 @@
+import React, { useState } from "react";
 import styles from "./ProductsCard.module.css";
 
 const ProductsCard = ({product}) => {
+
+    let [arr, setArr] = useState([]);
+    const [count, setCount] = useState(1);
+    const [price, setPrice] = useState(product.price);
+
+
+    const add = () => {
+        setArr((prev) => [...prev, product]);
+        setCount(count + 1);
+        setPrice(price + product.price);
+    }
     
+    const remove = () => {
+        if (count > 1) {
+            setArr((prev) => {
+                const newArr = [...prev]
+                newArr.pop()
+                return newArr
+            })
+            setCount(count - 1)
+            setPrice(price - product.price)
+        }
+    }
 
     return (
         <div className={styles.productscard}>
             <img src={product.image} alt={product.title} />
             <h3>{product.title}</h3>
-            <p>{product.price} $</p>
+            <p>{price} $</p>
             <p className={styles.rating}>
                 ⭐️ {product.rating.rate} <span>({product.rating.count})</span>
             </p>
+            <div className={styles.buttons}>
+                <button onClick={remove}>-</button>
+                <p className={styles.count}>{count}</p>
+                <button onClick={add}>+</button>
+            </div>
         </div>
     );
 };
